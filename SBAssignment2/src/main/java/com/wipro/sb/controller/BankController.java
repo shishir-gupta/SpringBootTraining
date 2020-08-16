a74ec0b52015130adc921d8d05b8c0feeb6a7b7b
shishir-gupta <68388098+shishir-gupta@users.noreply.github.com>
2020-08-15 21:19:15 -0400

    Add files via upload

--- SBAssignment2/src/main/java/com/wipro/sb/controller/BankController.java
+++ SBAssignment2/src/main/java/com/wipro/sb/controller/BankController.java
@@ -1,12 +1,19 @@
 package com.wipro.sb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wipro.sb.healthMonitor.CustomHealthMonitor;
 
 
 @Controller
 public class BankController {
-
+	
+	@Autowired
+	CustomHealthMonitor customHealthMonitor;
 	
 	@GetMapping("/bankBranch")
 	public String getBankName() {
@@ -17,4 +24,17 @@ public class BankController {
 	public String getBankAddress() {
 		return "bankBranchService";
 	}
+	
+	@PostMapping("/bringApplicationUp")
+	@ResponseBody
+	public String bringApplicationUp() {
+		return customHealthMonitor.bringApplicationUp();
+	}
+
+	@PostMapping("/bringApplicationDown")
+	@ResponseBody
+	public String bringApplicationDown() {
+		return customHealthMonitor.bringApplicationDown();
+	}
+
 }
